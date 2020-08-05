260b9db4fd5f2419c3971976d2418f373d26e4ee
Greenbirch2007 <291109028@qq.com>
2020-08-05 14:45:11 +0800

    增加汉字提取整理

---
+++ Google_trans/CleanSentence.py
@@ -0,0 +1,36 @@
+https://translate.google.cn/translate_tts?ie=UTF-8&q={}&tl=ja&total=1&idx=0&textlen=21&tk=454602.11157&client=webapp&prev=input
+https://translate.google.cn/#view=home&op=translate&sl=auto&tl=en&text={0}
+
+{0} 查询内容
+
+
+# 得使用抓包工具
+scapy,fiddler,wireshark
+
+https://translate.google.cn/#view=home&op=translate&sl=auto&tl=en&text={0}
+
+
+https://translate.google.cn/translate_tts?ie=UTF-8&q={0}&tl=ja&total=1&idx=0&textlen=24&tk=377638.198521&client=webapp&prev=input
+
+
+https://translate.google.cn/translate_tts?ie=UTF-8&q={}&tl=ja&total=1&idx=0&textlen=21&tk=454602.11157&client=webapp&prev=input
+https://translate.google.cn/#view=home&op=translate&sl=auto&tl=en&text={0}
+
+{0} 查询内容
+
+
+# 得使用抓包工具
+scapy,fiddler,wireshark
+
+https://translate.google.cn/#view=home&op=translate&sl=auto&tl=en&text={0}
+
+
+https://translate.google.cn/translate_tts?ie=UTF-8&q={0}&tl=ja&total=1&idx=0&textlen=24&tk=377638.198521&client=webapp&prev=input
+
+
+ re.sub(r"\[(.*?)\]",'',a)
+'いくら 安くたって、 好きじゃないものは 買わない。'
+
+
+いくら 安[やす]くたって、 好[す]きじゃないものは 買[か]わない。
+
